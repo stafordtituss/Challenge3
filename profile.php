246eac2b2a8c1894d90c551354d95d0a41d65320
Staford Titus S <stafordtituss@gmail.com>
2023-12-12 16:21:29 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,73 +1,91 @@
-<?php
-// We need to use sessions, so you should always start sessions using the below code.
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-	header('Location: index.html');
-	exit;
-}
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = 'toor';
-$DATABASE_NAME = 'phplogin';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-// We don't have the password or email info stored in sessions, so instead, we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
-// In this case we can use the account ID to get the account info.
-$stmt->bind_param('i', $_SESSION['id']);
-$stmt->execute();
-$stmt->bind_result($password, $email);
-$stmt->fetch();
-$stmt->close();
-?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Car Dashboard</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Profile Page</h2>
-			<div>
-				<p>Your account details are below:</p>
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><?=$_SESSION['name']?></td>
-					</tr>
-					<tr>
-						<?php
-$entry = 'chal2.log';
-if(isset($_COOKIE['admin'])) {
-if($_COOKIE['admin']=='yes'){
-    echo "<td><p><a href='download.php?file=".$entry."'>Download CANDump</a></p> </tr>";
-}
-}
-?>
-					</tr>
-					<tr>
-						<td>Password:</td>
-						<td><?=$password?></td>
-					</tr>
-					<tr>
-						<td>Email:</td>
-						<td><?=$email?></td>
-					</tr>
-				</table>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Randmob | Car user Dashboard</title>
+	<!--Fontawesome-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap--->
+    <link href="css/bootstrap-4.3.1.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/mainstyle.css">
+  </head>
+  <body>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div id="dashboard-hearder">
+					<div class="ml-4 pt-4">
+						<img src="images/Ellipse 9.png">
+						<p class="d-inline font-weight-bolder text-white">
+							Annabel Mark
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
-	</body>
+		<div class="row">
+			<div class="col-12">
+				<p id="amount">Amount</p>
+				<div id="amount-card">
+					<p class="ml-3 font-weight-bolder">
+					   <span id="money-value">500,0000</span> Naira
+					</p>
+				</div>
+			</div>
+		</div>
+		<!---Card Starts Here---->
+		<div class="row">
+			<div class="col" id="medcards-container">
+				<div class="medcard">
+					<p class="m-4 text-center">
+					  <img src="images/carbon_user-profile.svg" alt="">&nbsp;&nbsp;&nbsp;<span>Profiles</span>
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <a href="family.html"><img src="images/family.svg">&nbsp;&nbsp;&nbsp;Family</a>
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <img src="images/carbon_user-profile.svg" alt="">&nbsp;&nbsp;&nbsp;BSure
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <img src="images/Vector.svg" alt="">&nbsp;&nbsp;&nbsp;Credit
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <img src="images/bx_bx-history.svg" alt="">&nbsp;&nbsp;&nbsp;History
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <img src="images/pay.svg" alt="">&nbsp;&nbsp;&nbsp;Pay
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <i class="fa fa-road"></i>&nbsp;&nbsp;&nbsp;Book Mechanic
+					</p>
+				</div>
+				<div class="medcard">
+					<p class="m-4 text-center pt-1">
+					  <i class="fa fa-gears"></i>&nbsp;&nbsp;&nbsp;Auto parts discounts
+					</p>
+				</div>
+			</div>	
+		</div>
+		<!---Card Ends Here------>
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <!-- Include all csompiled plugins (below), or include individual files as needed -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.3.1.js"></script>
+  </body>
 </html>
